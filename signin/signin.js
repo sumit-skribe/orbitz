7278a496319a471e3a206f6aa8eebdf358252730
MD Afzal <101742037+afzal9632@users.noreply.github.com>
2022-07-06 15:59:56 +0530

    Update signin.js

--- signin/signin.js
+++ signin/signin.js
@@ -6,25 +6,27 @@ var signData = JSON.parse(localStorage.getItem("userlocal"));
 
 document.querySelector(".ak_btn").addEventListener("click",myfunction);
 
-function myfunction(){
+function myfunction()
+{
     var em1 = document.querySelector("#email8").value
      var pas1 = document.querySelector("#pass8").value
 
-    for(var i = 0;i<signData.length;i++){
-        if(signData[i].userEmail==em1 && signData[i].userPass==pas1){
+    for(var i = 0;i<signData.length;i++)
+    {
+        if(signData[i].userEmail==em1 && signData[i].userPass==pas1)
+        {
             // alert("Login Successful");   
-            window.location.href="../signup/signup.html"
-            break;
-            
+           return window.location.href="../index.html" 
         }
-        else{
-            swal({
-                title: "Warning",
-                text: "wrong email or passward",
-                icon: "error",
-                button: "ok",
-              });
-            break;
-        }
+    }  
+     swal({
+            title: "Warning",
+            text: "wrong email or passward",
+            icon: "error",
+            button: "ok",
+            });
     }
-    }
+    
+    function homePage()
+{
+    window.location.href = "../index.html"
